acb81763f5ba4e83a349a2f35690d05ec6c7be6e
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2021-02-24 20:14:08 -0300

    Aula 2 iniciada

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,11 +1,13 @@
-
+import React from 'react';
+import { ExperienceBar } from './components/ExperienceBar';
 import './styles/global.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
+      <ExperienceBar />
     </div>
-  );
+  )
 }
 
 export default App;
