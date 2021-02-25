0863fb506aad183a32114eaf1a9919873a16063b
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2021-02-24 23:48:53 -0300

    aula 03 : componentizacao

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -3,10 +3,12 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css'
+
 
 export default function Home() {
   return (
@@ -24,7 +26,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
